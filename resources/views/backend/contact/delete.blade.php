8046afb587ff53ffcc7536d7c0283d6c734d1c94
LeCongThang <hoavosac07101995@gmail.com>
2018-09-06 10:02:13 +0700

    Update

--- resources/views/backend/contact/delete.blade.php
+++ resources/views/backend/contact/delete.blade.php
@@ -1,10 +1,10 @@
-<div class="modal fade" id="modal_DeleteNews">
+<div class="modal fade" id="modal_Deletecontact">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title">Xác nhận xóa Tin tức</h4>
+                <h4 class="modal-title">Xác nhận xóa thông tin liên hệ</h4>
             </div>
             <div class="modal-body">
                 <table class="table table-bordered table-striped">
